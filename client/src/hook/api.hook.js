09e472f74d3030d36d2fb1f488bfe349b19e1210
Sherkhan <schmetterhs@gmail.com>
2020-09-07 19:37:52 +0500

    fixing client

--- client/src/hook/api.hook.js
+++ client/src/hook/api.hook.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export const useApi = () => {
 
     const callApi = async (endpoint, data, method="GET", headers)=>{
-        endpoint = 'http://127.0.0.1:5000' + endpoint
         console.log(method, endpoint, data);
         const userData = JSON.parse(localStorage.getItem('userData'));
         if(userData) {
